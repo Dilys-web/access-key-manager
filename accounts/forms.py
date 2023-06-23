29f02dbfbf63a96bfd7d2684b27c1894d8e1bbcb
Dilys Opoku-Donkor <opokudonkord@gmail.com>
2023-06-23 07:52:01 +0000

    forms for Login and Signup

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(attrs={"autofocus": True}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"class":'form-control'}))
+
+class AccountCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = get_user_model()
+        fields = ("email", "password1", "password2")
