987da43be2c0685c72bd0bf33d0b9e85adb7a029
Dilys Opoku-Donkor <opokudonkord@gmail.com>
2023-06-23 20:18:14 +0000

    Access key endpoint and key revoke

--- keys/urls.py
+++ keys/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.access_keys_list, name='home'),
-    path('new-key/', views.request_access_key, name='new_key')
+    path("", views.access_keys_list, name="home"),
+    path("new-key/", views.request_access_key, name="new_key"),
+    path("revoke-key/<int:access_key_id>/", views.revoke_access_key, name="revoke_key"),
+    path("api/access-keys/<str:email>/", views.get_active_access_key, name="active_access_key"),
 ]
--- keys/views.py
+++ keys/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import messages
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAdminUser
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.response import Response
 
+from .serializers import AccessKeySerializer
 from .models import AccessKey
-from .forms import AccessKeyRequestForm
 
+
+@login_required
 def access_keys_list(request):
     if request.user.is_staff:
         access_keys = AccessKey.objects.all()
@@ -18,17 +27,45 @@ def access_keys_list(request):
 @login_required
 def request_access_key(request):
     user = request.user
-    active_keys = AccessKey.objects.filter(user=user, status='active')
+    active_keys = AccessKey.objects.filter(user=user, status="active")
 
     if active_keys.exists():
         messages.error(request, "A user can have only one active key at a time.")
-        return redirect('home')  # Redirect to the access keys list if user has an active key
+        return redirect("home")  # Redirect to the access keys list if user has an active key
     
     try:
         new_key = AccessKey(user=user)
         new_key.save()
         messages.success(request, "New Access key successfully generated")
-        return redirect('home')
+        return redirect("home")
     except Exception as err:
-        messages.error(request,str(err))
-        return redirect('home')
+        messages.error(request, str(err))
+        return redirect("home")
+    
+
+@staff_member_required
+@login_required
+def revoke_access_key(request, access_key_id):
+    access_key = get_object_or_404(AccessKey, pk=access_key_id)
+
+    if access_key.status == "active":
+        access_key.status = "revoked"
+        access_key.save()
+        messages.success(request, f"Key: {access_key.key} successfully revoked")
+        return redirect("home")
+    messages.error(request, f"Key:{access_key.key} is not active")    
+    return redirect("home")
+
+   
+# EndPoint
+@api_view(["GET"])
+@authentication_classes([BasicAuthentication,])
+@permission_classes([IsAdminUser,])
+def get_active_access_key(request, email):
+    try:
+        access_key = AccessKey.objects.get(user__email=email, status="active")
+        serializer = AccessKeySerializer(access_key)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    except AccessKey.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
