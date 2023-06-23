b0a2da6b56ca07fbfb8721eb3e5f88b0f784af2e
Dilys Opoku-Donkor <opokudonkord@gmail.com>
2023-06-23 12:28:50 +0000

    add verification code

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import User
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ("email", "is_staff", "is_active")
+    list_display = ("email", "is_staff", "is_active", "verification_code")
     list_filter = ("is_staff", "is_active")
     search_fields = ("email",)
     ordering = ("email",)
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(User, CustomUserAdmin)
 
