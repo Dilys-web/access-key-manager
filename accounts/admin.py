e6628496749c0001ab6fc420d367356a832e8245
Dilys Opoku-Donkor <opokudonkord@gmail.com>
2023-06-23 22:25:23 +0000

    Fixed User creation error

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,10 +3,20 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ("email", "is_staff", "is_active", "verification_code")
-    list_filter = ("is_staff", "is_active")
-    search_fields = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
 
 admin.site.register(User, CustomUserAdmin)
-
